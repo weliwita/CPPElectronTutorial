eee5a69ad4335c72be9c3065bca5ae52c415dc94
Rasika Weliwita <rasika.weliwita@cslworld.com>
2017-07-21 13:57:48 +0530

    run file

---
+++ dotnetcore/ClrHostingApp/run.bat
@@ -0,0 +1,5 @@
+#https://github.com/dotnet/docs/tree/master/samples/core/hosting
+
+msbuild SampleHost.vcxproj
+
+x64\Debug\SampleHost.exe D:\Projects\GitHub\CPPElectronTutorial\dotnetcore\hello\bin\x64\Debug\netstandard2.0\hello.dll
